9f961cae9348d79f4157dd9f6c08eba343ed5c22
seo-bo <fgxhmd@gmail.com>
2025-02-02 02:43:01 +0900

    update

---
+++ LCA.cpp
@@ -0,0 +1,97 @@
+class LCA
+{
+private:
+	int n, P;
+	vector<int>depth;
+	vector<ll>dist;
+	vector<vector<int>>table;
+	vector<vector<pair<int, ll>>>graph;
+	void psh(int a, int b, ll w)
+	{
+		graph[a].push_back(make_pair(b, w));
+		graph[b].push_back(make_pair(a, w));
+	}
+	void dfs(int d, int parent, int node)
+	{
+		table[d][node] = parent;
+		depth[node] = d;
+		for (auto& [a, b] : graph[node])
+		{
+			if (a == parent)
+			{
+				continue;
+			}
+			dist[a] = dist[node] + b;
+			dfs(d + 1, node, a);
+		}
+	}
+	void build(int root)
+	{
+		dfs(0, 0, root);
+		for (int i = 1; i < P; ++i)
+		{
+			for (int j = 1; j <= n; ++j)
+			{
+				int mid = table[i - 1][j];
+				table[i][j] = table[i - 1][mid];
+			}
+		}
+	}
+	int lca(int a, int b)
+	{
+		if (depth[a] < depth[b])
+		{
+			swap(a, b);
+		}
+		int diff = depth[a] - depth[b];
+		for (int i = 0; diff; ++i, diff >>= 1)
+		{
+			if (diff & 1)
+			{
+				a = table[i][a];
+			}
+		}
+		if (a == b)
+		{
+			return a;
+		}
+		for (int i = P - 1; i >= 0; --i)
+		{
+			if (table[i][a] != table[i][b])
+			{
+				a = table[i][a];
+				b = table[i][b];
+			}
+		}
+		return table[0][a];
+	}
+	ll distance(int a, int b)
+	{
+		return dist[a] + dist[b] - 2 * dist[lca(a, b)];
+	}
+public:
+	LCA(int len)
+	{
+		n = len, P = 20;
+		depth.resize(n + 1, 0);
+		table.resize(P, vector<int>(n + 1));
+		dist.resize(n + 1, 0);
+		graph.resize(n + 1);
+	}
+	void add_edge(int u, int v, ll w = 0)
+	{
+		psh(u, v, w);
+	}
+	void init(int root = 1)
+	{
+		build(root);
+	}
+	int ancestor(int u, int v)
+	{
+		return lca(u, v);
+	}
+	ll get_dist(int u, int v)
+	{
+		return distance(u, v);
+	}
+};
---
+++ Segtree.cpp
@@ -0,0 +1,59 @@
+class Segtree
+{
+private:
+	int n;
+	ll MOD;
+	vector<ll>tree;
+	ll query_(int l, int r)
+	{
+		ll left = 0, right = 0;
+		l += n, r += n;
+		while (l < r)
+		{
+			if (l & 1)
+			{
+				left = (left + tree[l++]) % MOD;
+			}
+			if (r & 1)
+			{
+				right = (right + tree[--r]) % MOD;
+			}
+			l >>= 1;
+			r >>= 1;
+		}
+		return (left + right) % MOD;
+	}
+	void update_(int index, ll delta)
+	{
+		index += n;
+		tree[index] = delta % MOD;
+		for (index >>= 1; index >= 1; index >>= 1)
+		{
+			tree[index] = (tree[index << 1] + tree[(index << 1) | 1]) % MOD;
+		}
+	}
+public:
+	Segtree(vector<ll>& v) : Segtree(v, LLONG_MAX) {}
+	Segtree(vector<ll>& v, ll mod)
+	{
+		n = v.size();
+		MOD = mod;
+		tree.resize(2 * n);
+		for (int i = 0; i < n; ++i)
+		{
+			tree[n + i] = v[i] % MOD;
+		}
+		for (int i = n - 1; i > 0; --i)
+		{
+			tree[i] = (tree[i << 1] + tree[(i << 1) | 1]) % MOD;
+		}
+	}
+	ll query(int left, int right)
+	{
+		return query_(left - 1, right);
+	}
+	void update(int idx, ll value)
+	{
+		update_(idx - 1, value);
+	}
+};
